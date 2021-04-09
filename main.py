73b8143c3a7c4cf5cb880926697e7ab58a3c5f8f
johnpbonner3 <johnpbonner72@gmail.com>
2021-04-08 20:45:04 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 4/8/21
+# Script that will ideally, take information from the Augusta.com, convert an html table into a csv which I can then use
+# to manipulate for a personal Master's "Fantasy Golf" application
+
+
+# First imports
+
+
+
+
+
+
+
+
+
+
+
+
